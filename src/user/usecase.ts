c5ebb6d8f55415a29b3e06c90cfd0344e398817e
FortunaArthur <arthur.trabalho2646@gmail.com>
2024-02-05 13:11:58 -0300

    Completed CRUD implementation

--- src/user/usecase.ts
+++ src/user/usecase.ts
@@ -1,4 +1,4 @@
-import { UserCreate, UserRepository, UserUpdate } from "./interface";
+import { User, UserCreate, UserRepository, UserUpdate } from "./interface";
 import { UserRepositoryPrisma } from "./repository";
 
 
@@ -9,6 +9,7 @@ class UserUseCase {
     }
   
     async create(data: UserCreate) {
+
       const verifyUserExists = await this.userRepository.findByEmail(data.email)
       if (verifyUserExists) {
         throw new Error("User ja existe")
@@ -17,18 +18,42 @@ class UserUseCase {
       await this.userRepository.create(data)
 
     }
-  
 
+    async get(): Promise<Array<{ name: string; email: string }>> {
 
-    async findAll() {
+      const userList = await this.userRepository.get();
+    
+      const simplifiedResults = userList.map(user => ({ name: user.name, email: user.email }));
+    
+      return simplifiedResults;
+    }
+    
+
+    async updatePassword(email: string, newPassword: string): Promise<void> {
+
+      const existingUser = await this.userRepository.findByEmail(email);
+      if (!existingUser) {
+        throw new Error("User not found");
+      }
+
+      await this.userRepository.update(email, {
+        email: existingUser.email,
+        name: existingUser.name,
+        password: newPassword,
+      });
     }
 
-    async update(id:string, data: UserUpdate) {
+    async delete(email: string): Promise<void> {
+      
+      const existingUser = await this.userRepository.findByEmail(email);
+    
+      if (!existingUser) {
+        throw new Error("Usuário não encontrado");
+      }
+    
+      await this.userRepository.delete(email);
     }
+    
+}
 
-    async delete(id:string) {
-    }
-
-  }
-  
-  export { UserUseCase };
+export { UserUseCase };
